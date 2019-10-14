fbc2cb83c3628a0801255a87fff79adc735d3c42
Administrator <390057892@qq.com>
2019-10-14 19:59:35 +0800

    修改bug

--- app/src/main/java/com/novel/read/widget/dialog/ReadSettingDialog.java
+++ app/src/main/java/com/novel/read/widget/dialog/ReadSettingDialog.java
@@ -26,6 +26,7 @@ import com.novel.read.widget.page.ReadSettingManager;
 import java.util.Arrays;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReadSettingDialog extends Dialog {
 
@@ -72,6 +73,7 @@ public class ReadSettingDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_setting);
+        ButterKnife.bind(this);
         setUpWindow();
         initData();
         initWidget();
